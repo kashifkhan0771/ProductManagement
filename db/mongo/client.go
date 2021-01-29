8491f35fc2a36f7e24477a68caf7cd65ddf12e94
kashif khan <kashif.khan@wanclouds.net>
2021-01-29 15:33:31 +0500

    DB Layer Added(Mongo)

---
+++ db/mongo/client.go
@@ -0,0 +1,84 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	uuid "github.com/satori/go.uuid"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/kashifkhan0771/ProductManagement/config"
+	"github.com/kashifkhan0771/ProductManagement/db"
+	domainErr "github.com/kashifkhan0771/ProductManagement/errors"
+	"github.com/kashifkhan0771/ProductManagement/models"
+)
+
+const (
+	productCollection = "products"
+)
+
+func init() {
+	db.Register("mongo", NewClient)
+}
+
+type client struct {
+	conn *mongo.Client
+}
+
+// NewClient initializes a mongo database connection.
+func NewClient(conf db.Option) (db.DataStore, error) {
+	uri := fmt.Sprintf("mongodb://%s:%s/?connect=direct", viper.GetString(config.DBHost), viper.GetString(config.DBPort))
+	log().Infof("initializing mongodb: %s", uri)
+	cli, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to db")
+	}
+
+	return &client{conn: cli}, nil
+}
+
+func (c *client) AddProduct(product *models.Product) (string, error) {
+	if product.ID != "" {
+		return "", errors.New("id is not empty")
+	}
+	product.ID = uuid.NewV4().String()
+	collection := c.conn.Database(viper.GetString(config.DBName)).Collection(productCollection)
+	if _, err := collection.InsertOne(context.TODO(), product); err != nil {
+		return "", errors.Wrap(err, "failed to add product")
+	}
+
+	return product.ID, nil
+}
+
+func (c *client) GetProductByID(id string) (*models.Product, error) {
+	var product *models.Product
+	collection := c.conn.Database(viper.GetString(config.DBName)).Collection(productCollection)
+	if err := collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&product); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, domainErr.NewAPIError(domainErr.NotFound, fmt.Sprintf("product: %s not found", id))
+		}
+	}
+
+	return product, nil
+}
+
+func (c *client) DeleteProduct(id string) error {
+	collection := c.conn.Database(viper.GetString(config.DBName)).Collection(productCollection)
+	if _, err := collection.DeleteOne(context.TODO(), bson.M{"_id": id}); err != nil {
+		return errors.Wrap(err, "failed to delete product")
+	}
+
+	return nil
+}
+
+func (c *client) UpdateProduct(product *models.Product) error {
+	collection := c.conn.Database(viper.GetString(config.DBName)).Collection(productCollection)
+	if _, err := collection.UpdateOne(context.TODO(), bson.M{"_id": product.ID}, bson.M{"$set": product}); err != nil {
+		return errors.Wrap(err, "failed to update product")
+	}
+
+	return nil
+}
---
+++ db/mongo/client_test.go
@@ -0,0 +1,165 @@
+package mongo
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/kashifkhan0771/ProductManagement/db"
+	"github.com/kashifkhan0771/ProductManagement/models"
+)
+
+func Test_client_AddProduct(t *testing.T) {
+	t.Parallel()
+	_ = os.Setenv("DB_PORT", "27017")
+	_ = os.Setenv("DB_HOST", "localhost")
+
+	type args struct {
+		product *models.Product
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Success - Add Product to db",
+			args: args{product: &models.Product{
+				ProductID: "Product 001",
+				Name: "product name 1",
+				Price: 2300,
+				Status: "OnSelling",
+				Quantity: 34,
+			}},
+			wantErr: false,
+
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewClient(db.Option{})
+			if c != nil {
+				_, err := c.AddProduct(tt.args.product)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("AddProduct() error = %v, wantErr %v", err, tt.wantErr)
+
+					return
+				}
+			}
+		})
+	}
+}
+
+func Test_client_DeleteHost(t *testing.T) {
+	t.Parallel()
+	_ = os.Setenv("DB_PORT", "27017")
+	_ = os.Setenv("DB_HOST", "localhost")
+	c, _ := NewClient(db.Option{})
+	product := &models.Product{ProductID: "Product 002", Name: "Product Name 2",
+		Price: 4500, Status: "OffSelling", Quantity: 12}
+	if c != nil {
+		_, _ = c.AddProduct(product)
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Success - delete product from db",
+			args:    args{id: product.ID},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if err := c.DeleteProduct(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteProduct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_client_GetProductByID(t *testing.T) {
+	t.Parallel()
+	_ = os.Setenv("DB_PORT", "27017")
+	_ = os.Setenv("DB_HOST", "localhost")
+
+	c, _ := NewClient(db.Option{})
+	product := &models.Product{ProductID: "Product 003", Name: "Product Name 3", Price: 1399, Status: "OnSelling",
+		Quantity: 340}
+	if c != nil {
+		_, _ = c.AddProduct(product)
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *models.Product
+		wantErr bool
+	}{
+		{
+			name:    "Success - get product from db",
+			args:    args{product.ID},
+			want:    product,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := c.GetProductByID(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetProduct() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetProduct() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_client_UpdateProduct(t *testing.T) {
+	t.Parallel()
+	_ = os.Setenv("DB_PORT", "27017")
+	_ = os.Setenv("DB_HOST", "localhost")
+
+	c, _ := NewClient(db.Option{})
+	product := &models.Product{ProductID: "Product 004", Name: "Product Update", Price: 799, Status: "OffSelling",
+		Quantity: 78}
+
+	updateProductID, _ := c.AddProduct(product)
+
+	type args struct {
+		host *models.Product
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Success - update product in db",
+			args: args{host: &models.Product{ID: updateProductID, ProductID: "Product 004",
+				Name: "Product Update", Price: 750, Status: "OnSelling", Quantity: 100}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		if c != nil {
+			if err := c.UpdateProduct(tt.args.host); (err != nil) != tt.wantErr {
+				t.Errorf("UpdateProduct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		}
+	}
+}
